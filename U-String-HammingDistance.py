7919220872e09a7a336f622e1e888c723a46df2a
Andres Kwan <>
2019-05-29 11:07:09 -0500

    [String Hamming exercises] - Udacity

---
+++ U-String-HammingDistance.py
@@ -0,0 +1,48 @@
+def hamming_distance(str1, str2):
+    """
+    - 1 identify if strings have the same length
+    return None if they are different
+    - compare both string with the same index
+    if they are different count
+    :param str1:
+    :param str2:
+    :return:
+    """
+    distance_counter = 0
+    if len(str1) != len(str2):
+        return None
+
+    for i in range(len(str1)):
+        if str1[i] != str2[i]:
+            distance_counter += 1
+    return distance_counter
+
+
+print("Pass" if (10 == hamming_distance('ACTTGACCGGG','GATCCGGTACA')) else "Fail")
+print("Pass" if (1 == hamming_distance('shove','stove')) else "Fail")
+print("Pass" if (None == hamming_distance('Slot machines', 'Cash lost in me')) else "Fail")
+print("Pass" if (9 == hamming_distance('A gentleman','Elegant men')) else "Fail")
+print("Pass" if (2 == hamming_distance('0101010100011101','0101010100010001')) else "Fail")
+
+
+def hamming_distance(str1, str2):
+
+    """
+    Calculate the hamming distance of the two strings
+
+    Args:
+       str1(string),str2(string): Strings to be used for finding the hamming distance
+    Returns:
+       int: Hamming Distance
+    """
+
+    if len(str1) == len(str2):
+        count = 0
+
+        for char in range(len(str1)):
+            if str1[char] != str2[char]:
+                count+=1
+
+        return count
+
+    return None
