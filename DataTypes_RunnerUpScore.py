c1dda9aebc26037f4aed8bbc030ed857e387d033
Andres Kwan <>
2019-05-28 19:19:09 -0500

    [DataTypes - RunnerUpScore ] refactored to work on python3

--- DataTypes_RunnerUpScore.py
+++ DataTypes_RunnerUpScore.py
@@ -1,11 +1,20 @@
-n = int(raw_input())
-arr = map(int, raw_input().split())
+# n = int(raw_input())
+# arr = map(int, raw_input().split())
+
+n = int(input())
+arr = map(int, input().split())
+
 
 if 2 <= n and n <= 10:
     arr = list(dict.fromkeys(arr))
     arr = sorted(arr)
 
 # print arr
-print arr[len(arr)-2]
+print(arr[len(arr)-2])
 
+# input
+# 5
+# 2 3 6 6 5
 
+# output
+# 5
