39eddb7316a0df3b2149745329ee6005df79412b
Andres Kwan <>
2019-05-31 15:45:27 -0500

    [conditionals] refactoring

--- HR-Conditionals.py
+++ HR-Conditionals.py
@@ -1,9 +1,9 @@
 if n % 2 == 1:
     print("Weird")
-if n %2 == 0 and (2<=n and n<=5):
+if n % 2 == 0 and (2 <= n and n<=5):
     print("Not Weird")
-if n %2 == 0 and (6<=n and n<=20):
+if n % 2 == 0 and (6 <= n and n<=20):
     print("Weird")
-if n %2 == 0 and 20<n:
+if n % 2 == 0 and 20 < n:
     print("Not Weird")
 
