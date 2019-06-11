78c99e824fc1cdf64efc9bbf08e048a838048723
Andres Kwan <>
2019-06-11 00:22:49 -0500

    [U-Linked_List Reverse] - replacing __init__(self), with __init__(head,tail,length) - removing append O(n) version

--- U-Linked_List_Reverse.py
+++ U-Linked_List_Reverse.py
@@ -5,11 +5,6 @@ class Node:
 
 
 class LinkedList:
-    # def __init__(self):
-    #     self.head = None
-    #     self.tail = self.head
-    #     self.length = 0
-
     def __init__(self, head=None, tail=None, length=0):
         self.head = head
         self.tail = tail
@@ -41,17 +36,6 @@ class LinkedList:
         :return:
         """
         return str([v for v in self])
-
-    def append(self, value):
-        if self.head is None:
-            self.head = Node(value)
-            return
-
-        node = self.head
-        while node.next:
-            node = node.next
-
-        node.next = Node(value)
 
     def prepend(self, value):
         """ Prepend a value to the beginning of the list. """
