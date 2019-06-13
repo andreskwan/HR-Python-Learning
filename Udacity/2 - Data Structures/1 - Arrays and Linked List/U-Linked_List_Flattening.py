2e96b848bb100c1b7e0788779883958f8ccc9be8
Andres Kwan <>
2019-06-13 17:08:43 -0500

    [U-LinkedList Refactored] REMOVING commented code

--- Udacity/2 - Data Structures/1 - Arrays and Linked List/U-Linked_List_Flattening.py
+++ Udacity/2 - Data Structures/1 - Arrays and Linked List/U-Linked_List_Flattening.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -371,29 +373,6 @@ def sort(unsorted_list):
                 insert_node_after(previous, value)
     return sorted_list
 
-    # else:
-    # identify order
-
-    # value = list.pop()
-    # sorted_list = LinkedList(head=Node(value))
-    # current = list.head
-    # new_value = list.head.next.value
-    # while current:
-    #     if current.value > next_node.value:
-    #         current.next = None
-    #         temporal = next_node            # 8 -> 1 -> ... | (9) -> 8 # to preserve the tail
-    #         current.next = temporal.next
-    #         next_node.next = current
-    #         # next_node.next = temporal.next
-    #         # temporal_node = current  # (9) -> 8 | 7 -> 9
-    #         # next_node = temporal_node     # 8 -> 1 | 9 -> 8
-    #         # next_node.next = None         # 8 -> None
-    #         # current = next_node      # 7 -> (8) -> None
-    #         # current.next = temporal_node.next  # (8) -> 8
-    #     current = next_node
-
-import random
-
 
 def test_sort():
     unsorted_list = [random.randint(1, 100000) for x in range(200)]
@@ -443,132 +422,3 @@ def merge(list1, list2):
     list1.tail.next = list2.head
     list1.tail = list2.tail
     return sort(list1)
-#     next_node = current_node.next
-
-# class NestedLinkedList(LinkedList):
-#     def flatten(self):
-#         """
-#
-#         :rtype: LinkedList
-#         """
-#         # TODO: Implement this method to flatten the linked list in ascending sorted order.
-#         pass
-#
-#
-# array = [1]
-# list1 = LinkedList(init_list=array)
-# list1.pop()
-#
-#
-# # # First Test scenario
-# # linked_list = LinkedList(head=Node(1))
-# # linked_list.append_value(3)
-# # linked_list.append_value(5)
-# # # linked_list.append_value(Node(3))
-# # # linked_list.append_value(Node(5))
-# #
-# # # To test merge
-# # # print(linked_list.to_list())
-# #
-# # linked_list2 = LinkedList(head=Node(7))
-# # linked_list2.append(Node(9))
-# # linked_list2.append(Node(8))
-# # # linked_list2.append_value(4)
-# # # linked_list2.append_value(8)
-# #
-# # merge(linked_list2, linked_list)
-# # print(linkedList3.to_list())
-#
-# # nested_linked_list = NestedLinkedList(Node(linked_list))
-# #
-# # second_linked_list = LinkedList(Node(2))
-# # second_linked_list.append_value(4)
-# #
-# # nested_linked_list.append_value(Node(second_linked_list))
-# #
-# # solution = nested_linked_list.flatten()
-# # assert solution == [1, 2, 3, 4, 5]
-
-# previous attempts
-#
-# def sort1(unsorted_list):
-#     if 0 == unsorted_list.length:
-#         return None
-#     if 1 == unsorted_list.length:
-#         return unsorted_list
-#     first = unsorted_list.pop()
-#     # second = unsorted_list.pop()
-#     sorted_list = LinkedList(Node(first))
-#     current = sorted_list.head
-#
-#     while unsorted_list.length > 0:  # if there is more to sort
-#         second = unsorted_list.pop()  # get head from unsorted
-#         if current.value < second:  # identify position of second with respect the current
-#             # where is current?
-#             # - at first in the head of the new sorted_list
-#             # then
-#             # -
-#             if current.next is not None:
-#                 # if there is a next node from current in the sorted_list
-#
-#                 if second < current.next.value:  # append to head // 2 < 80
-#                     insert_node_after(current, second)
-#                     sorted_list.head = current
-#                 else:
-#                     # if current.next.value < second:
-#                     #     # append to head.next --
-#                     #     third = Node(second)
-#                     #     third.next = current.next.next
-#                     #     current.next.next = third
-#                     # else:
-#                     # sorted_list.prepend(second)  # 1->2->3 |
-#                     insert_node_after(current.next, second)
-#                     sorted_list = sort(sorted_list)
-#             else:
-#                 if current.next is not None:  # must be deleted
-#                     if second < current.next.value:
-#                         insert_node_after(current, second)
-#                     else:
-#                         sorted_list.append_value(second)
-#                         # current = current.next
-#                 else:
-#                     sorted_list.append_value(second)
-#         else:  # insert in the head of sorted_list
-#             # why not before current?
-#             # current is not always head
-#             # sorted_list.prepend(second)
-#             sorted_list.prepend(second)
-#             # current = sorted_list.head
-#             # sorted_list = sort(sorted_list)
-#     return sorted_list
-#
-#
-# def sort2(unsorted_list):
-#     if 0 == unsorted_list.length:
-#         return None
-#     if 1 == unsorted_list.length:
-#         return unsorted_list
-#     first = unsorted_list.pop()
-#     # second = unsorted_list.pop()
-#     sorted_list = LinkedList(Node(first))
-#     current = sorted_list.head
-#
-#     while unsorted_list:  # if there is more to sort
-#         while current:
-#             second = unsorted_list.pop()  # get head from unsorted
-#             if current.next:
-#                 if second < current.value:
-#                     insert_node_after(current, second)  # 2->1
-#                     swap_data(current, current.next)  # 1->2
-#                 if current.value < second < current.next.value:
-#                     insert_node_after(current, second)
-#                 # compare and search where it should be inserted
-#                 if current.next.value < second:
-#                     insert_node_after(current.next, second)
-#             else:
-#                 if current.value < second:
-#                     sorted_list.append_value(second)
-#                 else:
-#                     insert_node_after(current, second)  # 2->1
-#                     swap_data(current, current.next)  # 1->2
-#             # current = current.next
