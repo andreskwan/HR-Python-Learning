6f1c19342a0043e453bacf56deae4e46447955c0
Andres Kwan <>
2019-06-28 18:40:11 -0500

    [U-Arrays/List Pascal] - with recursivity, didn't work on Jupiter. Why? because the start they start the triangle from n = 0 not from n = 1

--- Udacity/2 - Data Structures/1 - Arrays and Linked List/Arrays/U-Arrays_Pascal.py
+++ Udacity/2 - Data Structures/1 - Arrays and Linked List/Arrays/U-Arrays_Pascal.py
@@ -1,4 +1,9 @@
-def nth_row_pascal(n):
+def nth_row_pascal_udacity(n):
+    """
+    first row is n = 0
+    :param n:
+    :return:
+    """
     n -= 1
     if n == 0:
         return [1]
@@ -6,44 +11,48 @@ def nth_row_pascal(n):
     for i in range(1, n + 1):
         previous_row = current_row
         current_row = [1]
-        for j in range(1, i):
+        for j in range(1, i):  # start from 1 allows to have (j - 1)
             next_number = previous_row[j] + previous_row[j - 1]
             current_row.append(next_number)
         current_row.append(1)
     return current_row
 
-# def nth_row_pascal(n):
-#     """
-#     :param - arr - input array
-#     return - number - largest sum in contiguous subarry within arr
-#     """
-#     n += 1
-#     if n == 1:
-#         return [1]
-#     if n == 2:
-#         return [1, 1]
-#
-#     array = [1]
-#
-#     p_r = nth_row_pascal(n-1)
-#     print(p_r)
-#     for i in range(n-2):
-#         value = p_r[i] + p_r[i+1]
-#         array.append(value)
-#     array.append(1)
-#     return array
+
+def nth_row_pascal(n):
+    """
+    first row is n = 1
+    :param - arr - input array
+    return - number - largest sum in contiguous subarry within arr
+    """
+    if n == 0:
+        return [1]
+    if n == 1:
+        return [1, 1]
+
+    current_row = [1]
+    previous_row = nth_row_pascal(n-1)
+    for i in range(n-1):
+        next_number = previous_row[i] + previous_row[i+1]
+        current_row.append(next_number)
+    current_row.append(1)
+    return current_row
 
 
-
+# (1, [1]),
+#         (2, [1, 1]),
+#         (3, [1, 2, 1]),
+#         (4, [1, 3, 3, 1]),
+#         (7, [1, 6, 15, 20, 15, 6, 1]),
+#         (9, [1, 8, 28, 56, 70, 56, 28, 8, 1])]
 
 def test_nth_row_pascal():
     test_cases = [
-        (1, [1]),
-        (2, [1, 1]),
-        (3, [1, 2, 1]),
-        (4, [1, 3, 3, 1]),
-        (7, [1, 6, 15, 20, 15, 6, 1]),
-        (9, [1, 8, 28, 56, 70, 56, 28, 8, 1])]
+        (0, [1]),
+        (1, [1, 1]),
+        (2, [1, 2, 1]),
+        (3, [1, 3, 3, 1]),
+        (6, [1, 6, 15, 20, 15, 6, 1]),
+        (8, [1, 8, 28, 56, 70, 56, 28, 8, 1])]
 
     for (args, answer) in test_cases:
         print("---------------------")
