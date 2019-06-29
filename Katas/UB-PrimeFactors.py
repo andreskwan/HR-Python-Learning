586c140bb57a3342b3848c1ee2dc2c190ae54135
Andres Kwan <>
2019-06-29 14:41:11 -0500

    [Kata - PrimeFactors] Completed

--- Katas/UB-PrimeFactors.py
+++ Katas/UB-PrimeFactors.py
@@ -2,31 +2,30 @@ def test_prime_factors():
     print("--------------------------")
     print("-------test_prime_factors-------")
     test_cases = [
-        (1, [])]
-        # ,
-        # (2, [2]),
-        # (3, [3]),
-        # (4, [2, 2]),
-        # (5, [5]),
-        # (6, [2, 3]),
-        # (7, [7]),
-        # (8, [2, 2, 2]),
-        # (9, [3, 3]),
-        # (10, [2, 5]),
-        # (11, [11]),
-        # (12, [2, 2, 3]),
-        # (13, [13]),
-        # (14, [2, 7]),
-        # ((2 * 3 * 3 * 5 * 7 * 11 * 11 * 13),[2, 2, 3, 3, 5, 7, 11, 11, 13])]
+        (1, []),
+        (2, [2]),
+        (3, [3]),
+        (4, [2, 2]),
+        (5, [5]),
+        (6, [2, 3]),
+        (7, [7]),
+        (8, [2, 2, 2]),
+        (9, [3, 3]),
+        (10, [2, 5]),
+        (11, [11]),
+        (12, [2, 2, 3]),
+        (13, [13]),
+        (14, [2, 7]),
+        ((2 * 3 * 3 * 5 * 7 * 11 * 11 * 13), [2, 3, 3, 5, 7, 11, 11, 13])]
 
     for (args, answer) in test_cases:
         print("---------------------")
         result = prime_factors(args)
 
         if result is not None and answer is not None:
-            print("input: " + str(args) + " | expected answer: " + str(answer.to_list()) + " | result: " + str(
-                result.to_list()))
-            if result.to_list() == answer.to_list():
+            print("input: " + str(args) + " | expected answer: " + str(answer) + " | result: " + str(
+                result))
+            if result == answer:
                 print("Test case passed!")
             else:
                 print("Test with input data:", args, "failed")
@@ -38,9 +37,15 @@ def test_prime_factors():
                 print("Test with input data:", args, "failed")
 
 
-def prime_factors(args):
-    return []
+def prime_factors(n):
+    factors_list = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors_list.append(divisor)
+            n //= divisor
+        divisor += 1
+    return factors_list
 
 
 test_prime_factors()
-
