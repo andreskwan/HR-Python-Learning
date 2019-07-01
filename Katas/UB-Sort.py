e15db3b6baf180f5351c128cfdfb8c54991f136d
Andres Kwan <>
2019-07-01 17:20:55 -0500

    [Kata - Sort - TPP] green and refactor for [2, 1, 3] and [2, 3, 1]

--- Katas/UB-Sort.py
+++ Katas/UB-Sort.py
@@ -2,15 +2,32 @@ import random
 
 
 def sort(array: [int]) -> [int]:
-    sorted_list = array
+    sorted_list = []
     size = len(array)
-    while size > 0:
-        index = 0
-        while size > index + 1:  # then swap
-            if is_out_of_order(array, index):
-                swap(array, index)
-            index += 1
-        size -= 1
+    if size == 1:
+        return sorted_list.append(array[0])
+    elif size == 2:
+        if array[0] > array[1]:
+            sorted_list.append(array[1])
+            sorted_list.append(array[0])
+        else:
+            sorted_list.append(array[0])
+            sorted_list.append(array[1])
+    elif size == 3:
+        lowest = 0
+        middle = array[0]
+        heights = 0
+
+        if array[1] > array[2]:
+            lowest = array[2]
+            heights = array[1]
+        else:
+            lowest = array[1]
+            heights = array[2]
+
+        sorted_list.append(lowest)
+        sorted_list.append(middle)
+        sorted_list.append(heights)
     return sorted_list
 
 
@@ -30,14 +47,16 @@ def test_sort():
     unsorted_list = [random.randint(1, 100000) for x in range(1000)]
     sorted_list = sorted(unsorted_list)
     test_cases = [
-        # ([], []),
-        # ([1], [1]),
-        # ([2, 1], [1, 2]),
-        # # ([1, 2, 3], [1, 2, 3]),
-        # # ([2, 1, 3], [1, 2, 3]),
+        ([], []),
+        ([1], [1]),
+        ([2, 1], [1, 2]),
+        # ([1, 2, 3], [1, 2, 3])]
+        ([2, 1, 3], [1, 2, 3]),
+        ([2, 3, 1], [1, 2, 3])]
+        # ([3, 1, 2], [1, 2, 3])]
         # ([1, 3, 2], [1, 2, 3]),
-        ([3, 2, 1], [1, 2, 3]),
-        (unsorted_list, sorted_list)]
+        # ([3, 2, 1], [1, 2, 3])]
+        # (unsorted_list, sorted_list)]
 
     for (args, answer) in test_cases:
         print("---------------------")
@@ -59,5 +78,3 @@ def test_sort():
 
 
 test_sort()
-
-
