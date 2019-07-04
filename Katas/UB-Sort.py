38310197f815a087c39cef94c18c22e0467be820
Andres Kwan <>
2019-07-03 21:09:24 -0500

    [Kata - Sort - TPP] completed TODO - fix sort sorted array recursion problem. - sorting 1000 elements is failing.

--- Katas/UB-Sort.py
+++ Katas/UB-Sort.py
@@ -1,27 +1,38 @@
 import random
 
+# def sort(array: [int]) -> [int]:
+#     sorted_list = []
+#     size = len(array)
+#     if size == 1:
+#         return array
+#     else:
+#     lowest = None
+#     middle = array[0]
+#     heights = None
+#
+#     for value in array:
+#         if value > middle:
+#             heights = value
+#         if value < middle:
+#             lowest = value
+#
+#     if lowest is not None:  sorted_list.append(lowest)
+#     sorted_list.append(middle)
+#     if heights is not None:  sorted_list.append(heights)
+#
+#     return sorted_list
 
-def sort(array: [int]) -> [int]:
-    sorted_list = []
-    size = len(array)
-    if size <= 1:
-        return array
 
-    lowest = None
-    middle = array[0]
-    heights = None
-
-    for value in array:
-        if value > middle:
-            heights = value
-        if value < middle:
-            lowest = value
-
-    if lowest is not None:  sorted_list.append(lowest)
-    sorted_list.append(middle)
-    if heights is not None:  sorted_list.append(heights)
-
-    return sorted_list
+# def sort(array):
+#     size = len(array)
+#     while size > 0:
+#         index = 0
+#         while size > index + 1:
+#             if is_out_of_order(array, index):
+#                 swap(array, index)
+#             index += 1
+#         size -= 1
+#     return array
 
 
 def is_out_of_order(args, index):
@@ -34,29 +45,74 @@ def swap(args, index):
     args[index + 1] = temp
 
 
+def sort(array):
+    sorted_list = []
+    size = len(array)
+
+    if size == 0:
+        return array
+    else:
+        middle = array[0]
+        lower = []
+        higher = []
+        for value in array:
+            if value > middle:
+                higher.append(value)
+            elif value < middle:
+                lower.append(value)
+
+        sorted_list.extend(sort(lower))
+        sorted_list.append(middle)
+        sorted_list.extend(sort(higher))
+    return sorted_list
+
+
+def swap_computed(array, middle):
+    lower = None
+    higher = None
+    for value in array:
+        if value > middle:
+            higher = value
+        elif value < middle:
+            lower = value
+    return higher, lower
+# def swap_computed(array, index):
+#     if array[index] > array[index + 1]:
+#         lower = array[index + 1]
+#         higher = array[index]
+#     else:
+#         lower = array[index]
+#         higher = array[index + 1]
+#     return higher, lower
+
+
 def test_sort():
     print("--------------------------")
     print("-------test_prime_factors-------")
-    unsorted_list = [random.randint(1, 100000) for x in range(1000)]
+    unsorted_list = [random.randint(1, 100000) for x in range(100)]
     sorted_list = sorted(unsorted_list)
+    # sorted_list = [*range(10000)]
     test_cases = [
         ([], []),
         ([1], [1]),
+        ([1, 2], [1, 2]),
         ([2, 1], [1, 2]),
-        # ([1, 2, 3], [1, 2, 3])]
-        ([2, 1, 3], [1, 2, 3]),
-        ([2, 3, 1], [1, 2, 3])]
-    # ([3, 1, 2], [1, 2, 3])]
-    # ([1, 3, 2], [1, 2, 3]),
-    # ([3, 2, 1], [1, 2, 3])]
-    # (unsorted_list, sorted_list)]
+        ([1, 2, 3], [1, 2, 3]),   # 6 permutations
+        ([1, 3, 2], [1, 2, 3]),
+        ([2, 1, 3], [1, 2, 3]),  # pivot point is array[0]
+        ([2, 3, 1], [1, 2, 3]),
+        ([3, 1, 2], [1, 2, 3]),
+        ([3, 2, 1], [1, 2, 3]),
+        # (sorted_list, sorted_list)]
+        (unsorted_list, sorted_list)]
 
     for (args, answer) in test_cases:
         print("---------------------")
+        unmodified = args[:]
         result = sort(args)
 
         if result is not None and answer is not None:
-            print("input: " + str(args) + " | expected answer: " + str(answer) + " | result: " + str(
+            print("input: " + str(unmodified) + " | expected answer: " + str(answer) + " | result: " + str(
                 result))
             if result == answer:
                 print("Test case passed!")
@@ -67,7 +123,7 @@ def test_sort():
             if result == answer:
                 print("Test case passed!")
             else:
-                print("Test with input data:", args, "failed")
+                print("Test with input data:", unmodified, "failed")
 
 
 test_sort()
