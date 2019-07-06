f6ef4bba2d65da8f3d008cdc7aa01aed9dfa2469
Andres Kwan <>
2019-07-06 10:48:18 -0500

    [Kata - 2 - Sort - TPP] Second kata I understand - the concept of pivot - why is important to select the pivot at index 0 //array[0] TODO - fix sort sorted array recursion problem.

--- Katas/UB-Sort.py
+++ Katas/UB-Sort.py
@@ -1,51 +1,29 @@
 import random
 
-
-# TODO - fix sort sorted array recursion problem.
-def sort(array):
-    sorted_list = []
-    lower = []
-    higher = []
-    pivot = None
-
-    if len(array) <= 1:
-        return array
-    elif len(array) > 1:
-        pivot = array[0]
-        for index, value in enumerate(array):
-            if pivot == value and 0 != index:
-                lower.append(value)
-            if pivot > value:
-                lower.append(value)
-            if pivot < value:
-                higher.append(value)
-
-    sorted_list.extend(sort(lower))
-    if pivot is not None: sorted_list.append(pivot)
-    sorted_list.extend(sort(higher))
-    return sorted_list
-
-
 def test_sort():
     print("--------------------------")
     print("-------test_prime_factors-------")
-    unsorted_list = [random.randint(1, 10) for x in range(100)]
-    sorted_list = sorted(unsorted_list)
+    repeated_values = [random.randint(1, 5) for x in range(10)]
+    repeated_sorted = sorted(repeated_values)
+    large = [random.randint(1, 1000000000) for x in range(1000)]
+    large_sorted = sorted(large)
     # sorted_list = [*range(10000)]
     test_cases = [
         ([], []),
         ([1], [1]),
         ([1, 2], [1, 2]),
         ([2, 1], [1, 2]),
-        ([1, 2, 3], [1, 2, 3]),  # 6 permutations
-        ([1, 3, 2], [1, 2, 3]),
-        ([2, 1, 3], [1, 2, 3]),  # pivot point is array[0]
-        ([2, 3, 1], [1, 2, 3]),
-        ([3, 1, 2], [1, 2, 3]),
-        ([3, 2, 1], [1, 2, 3]),
+        ([1, 2, 3], [1, 2, 3]),  # middle
+        ([3, 2, 1], [1, 2, 3]),  # middle    - pivot at array[1]
+        ([2, 1, 3], [1, 2, 3]),  # beginning - pivot at array[0]
+        ([2, 3, 1], [1, 2, 3]),  # beginning
+        ([1, 3, 2], [1, 2, 3]),  # end       - pivot at array[2]
+        ([3, 1, 2], [1, 2, 3]),  # end
+        (repeated_values, repeated_sorted),
         ([3, 2, 2, 1], [1, 2, 2, 3]),
-        # (sorted_list, sorted_list)]  # to test sorting a sorted array
-        (unsorted_list, sorted_list)]  # to test sorting an array with many repeated values
+        (large, large_sorted),
+        (large_sorted, large_sorted),  # works up to 1000 values - recursive stack size
+    ]
 
     for (args, answer) in test_cases:
         print("---------------------")
@@ -62,5 +40,29 @@ def test_sort():
         else:
             print("Test with input data:", unmodified, "failed")
 
+
+# TODO - fix sort sorted array recursion problem.
+def sort(array):
+    sorted_list = []
+    lower = []
+    higher = []
+    pivot = None
+
+    if len(array) <= 1:
+        return array
+    if len(array) > 1:
+        pivot = array[0]
+        for index, value in enumerate(array):
+            if value == pivot and 0 != index:
+                higher.append(value)
+            if value < pivot:
+                lower.append(value)
+            elif value > pivot:
+                higher.append(value)
+
+    sorted_list.extend(sort(lower))
+    sorted_list.append(pivot)
+    sorted_list.extend(sort(higher))
+    return sorted_list
 
 test_sort()
