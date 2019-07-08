1bfd3838d61ff65231c84e10ea4b729d0fbd091b
Andres Kwan <>
2019-07-08 11:52:15 -0500

    [Kata - Sort - TPP] My third kata I understood: - base case for recursive approach - when a variable should transform into an array
    TODO
    - fix sort sorted array recursion problem.

--- Katas/UB-Sort.py
+++ Katas/UB-Sort.py
@@ -1,27 +1,28 @@
 import random
 
+
 def test_sort():
     print("--------------------------")
     print("-------test_prime_factors-------")
     repeated_values = [random.randint(1, 5) for x in range(10)]
     repeated_sorted = sorted(repeated_values)
-    large = [random.randint(1, 1000000000) for x in range(1000)]
+    large = [random.randint(1, 1000000000) for x in range(2000)]
     large_sorted = sorted(large)
     # sorted_list = [*range(10000)]
     test_cases = [
-        ([], []),
-        ([1], [1]),
-        ([1, 2], [1, 2]),
-        ([2, 1], [1, 2]),
-        ([1, 2, 3], [1, 2, 3]),  # middle
-        ([3, 2, 1], [1, 2, 3]),  # middle    - pivot at array[1]
-        ([2, 1, 3], [1, 2, 3]),  # beginning - pivot at array[0]
-        ([2, 3, 1], [1, 2, 3]),  # beginning
-        ([1, 3, 2], [1, 2, 3]),  # end       - pivot at array[2]
-        ([3, 1, 2], [1, 2, 3]),  # end
-        (repeated_values, repeated_sorted),
-        ([3, 2, 2, 1], [1, 2, 2, 3]),
-        (large, large_sorted),
+        # ([], []),
+        # ([1], [1]),
+        # ([1, 2], [1, 2]),
+        # ([2, 1], [1, 2]),
+        # ([1, 2, 3], [1, 2, 3]),  # middle
+        # ([3, 2, 1], [1, 2, 3]),  # middle    - pivot at array[1] - TPP - constant to array - lower = []
+        # ([2, 1, 3], [1, 2, 3]),  # beginning - pivot at array[0]
+        # ([2, 3, 1], [1, 2, 3]),  # beginning
+        # ([1, 3, 2], [1, 2, 3]),  # end       - pivot at array[2] - TPP - constant to array - higher = []
+        # ([3, 1, 2], [1, 2, 3]),  # end
+        # (repeated_values, repeated_sorted),
+        # ([3, 2, 2, 1], [1, 2, 2, 3]),
+        # (large, large_sorted),
         (large_sorted, large_sorted),  # works up to 1000 values - recursive stack size
     ]
 
@@ -44,25 +45,26 @@ def test_sort():
 # TODO - fix sort sorted array recursion problem.
 def sort(array):
     sorted_list = []
+    pivot = None
     lower = []
     higher = []
-    pivot = None
-
-    if len(array) <= 1:
+    size = len(array)
+    if size <= 1:  # <= 1 to have the base case for the recursive version.
         return array
-    if len(array) > 1:
+    if size > 1:
         pivot = array[0]
         for index, value in enumerate(array):
-            if value == pivot and 0 != index:
+            if index != 0 and value == pivot:
                 higher.append(value)
-            if value < pivot:
+            if value > pivot:
+                higher.append(value)
+            elif value < pivot:
                 lower.append(value)
-            elif value > pivot:
-                higher.append(value)
 
     sorted_list.extend(sort(lower))
     sorted_list.append(pivot)
     sorted_list.extend(sort(higher))
     return sorted_list
+
 
 test_sort()
