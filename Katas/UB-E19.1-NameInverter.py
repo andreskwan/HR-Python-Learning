417bf92f87319b3bea0c9663c4358bb4663364da
Andres Kwan <>
2019-07-13 17:31:21 -0500

    [TDD - E19.1 -  NameInverter] - Refactoring

--- Katas/UB-E19.1-NameInverter.py
+++ Katas/UB-E19.1-NameInverter.py
@@ -9,28 +9,35 @@ def is_honorific(word):
     return re.search(r"^(Mr\.|Mrs\.)", word)
 
 
-def get_postNominals(param):
+def get_post_nominals(param):
     postnominals = ""
     for x in param[2:]:
         postnominals += x + " "
     return postnominals
 
 
+def formatted_name(names, postnominals):
+    reversed_named = "" + names[1] + ", " + names[0] + " " + postnominals
+    return reversed_named.strip()
+
+
+def remove_honorifics(names):
+    if len(names) > 1 and is_honorific(names[0]):
+        names.pop(0)
+
+
 def name_inverter(name):
     if name is None:
         return ""
-    # TPP 6 variable to array
     names = split_name(name)
-    if len(names) > 1 and is_honorific(names[0]):
-        names.pop(0)
+    remove_honorifics(names)
     if len(names) == 1:
         return names[0]
     else:
         postnominals = ""
         if len(names) > 2:
-            postnominals = get_postNominals(names)
-        reversed_name = "" + names[1] + ", " + names[0] + " " + postnominals
-        return reversed_name.strip()
+            postnominals = get_post_nominals(names)
+        return formatted_name(names, postnominals)
 
 
 def test_name_inverter():
@@ -38,19 +45,20 @@ def test_name_inverter():
     print("-------test_prime_factors-------")
     test_cases = [
         # degenerate cases first
-        (None, ""),  # givenNull_returnEmptyString
-        ("", ""),  # givenEmptyString_returnEmptyString
-        ("Name", "Name"),  # givenOnlyName_returnOnlyName
+        (None, ""),                                                 # givenNull_returnEmptyString
+        ("", ""),                                                   # givenEmptyString_returnEmptyString
+        ("Name", "Name"),                                           # givenOnlyName_returnOnlyName
         # specific cases
         # - spaces
-        ("  Name    ", "Name"),  # givenOnlyNameWithSpaces_returnOnlyNameWithoutSpaces
-        ("   First    Last   ", "Last, First"),  # givenFirstLastWithSpaces_returnLastFirst
+        ("  Name    ", "Name"),                                     # givenOnlyNameWithSpaces_returnOnlyNameWithoutSpaces
+        ("   First    Last   ", "Last, First"),                     # givenFirstLastWithSpaces_returnLastFirst
         # -
-        ("First Last", "Last, First"),  # givenFirstLast_returnLastFirst
-        ("Mr. First Last", "Last, First"),  # ignoreHonorifics
-        ("Mrs. First Last", "Last, First"),  # ignoreHonorific
-        ("First Last Sr.", "Last, First Sr."),  # postNominal_stayAtEnd
-        ("First Last Sr. BS. Phd.", "Last, First Sr. BS. Phd."),  # postNominal_stayAtEnd
+        ("First Last", "Last, First"),                              # givenFirstLast_returnLastFirst
+        ("Mr. First Last", "Last, First"),                          # ignoreHonorifics
+        ("Mrs. First Last", "Last, First"),                         # ignoreHonorific
+        ("First Last Sr.", "Last, First Sr."),                      # postNominal_stayAtEnd
+        ("First Last Sr. BS. Phd.", "Last, First Sr. BS. Phd."),    # postNominals_stayAtEnd
+        ("     First      Last     Sr.    BS.    Phd.   ", "Last, First Sr. BS. Phd."),    # integration
 
     ]
 
