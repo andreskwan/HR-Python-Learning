ff7b2af821188b61cbca535da5bb84e97f922100
Andres Kwan <>
2019-07-16 11:55:32 -0500

    [U-Linked_List Even-After-Odd-Nodes] - Before refactor cases working (None, []), ([], []), ([1], [1]),
    ([1, 3], [1, 3]),             # only odds
    ([3, 1], [3, 1]),             # only odds
    
    ([2, 4], [2, 4]),           # only even
    ([4, 2], [4, 2]),           # only even
    
    ([1, 2], [1, 2]),           # even after odd
    ([2, 1], [1, 2]),           # even before odd

--- Udacity/2 - Data Structures/1 - Arrays and Linked List/LinkedList/U-Linked_List-Even-After-Odd-Nodes.py
+++ Udacity/2 - Data Structures/1 - Arrays and Linked List/LinkedList/U-Linked_List-Even-After-Odd-Nodes.py
@@ -6,6 +6,8 @@ class Node:
 
 # helper functions for testing purpose
 def create_linked_list(arr):
+    if arr is None:
+        return None
     if len(arr) == 0:
         return None
     head = Node(arr[0])
@@ -24,6 +26,8 @@ def print_linked_list(head):
 
 
 def to_list(head):
+    if head is None:
+        return None
     out = []
     node = head
     while node:
@@ -32,70 +36,87 @@ def to_list(head):
     return out
 
 
-# Solution
-def even_after_odd(current_node):
-    if current_node is None:
-        return create_linked_list([])
-
+def even_after_odd(head):
+    if head is None:
+        return []
+    current_node = head
+    node_next = None
     even = None
     odd = None
+    odd_tail = None
     even_tail = None
-    head_tail = None
 
-    while current_node:
-        next_node = current_node.next
-
+    if current_node is not None:
+        node_next = current_node.next
+        current_node.next = None
         if current_node.data % 2 == 0:
             if even is None:
                 even = current_node
-                even_tail = even
+                even_tail = current_node
             else:
                 even_tail.next = current_node
-                even_tail = even_tail.next
+                even_tail = current_node
         else:
             if odd is None:
                 odd = current_node
-                odd_tail = odd
+                odd_tail = current_node
             else:
                 odd_tail.next = current_node
-                odd_tail = odd_tail.next
-        current_node.next = None
-        current_node = next_node
-
+                odd_tail = current_node
+    if node_next is not None:
+        if node_next.data % 2 == 0:
+            if even is None:
+                even = node_next
+                even_tail = node_next
+            else:
+                even_tail.next = node_next
+                even_tail = node_next
+        else:
+            if odd is None:
+                odd = node_next
+                odd_tail = node_next
+            else:
+                odd_tail.next = node_next
+                odd_tail = node_next
+    if odd_tail is not None:
+        odd_tail.next = even
+    # if even_tail is not None:
+    #     even_tail.next = even
     if odd is None:
         return even
-    odd_tail.next = even
     return odd
-
-
-# return list/head not an array
-# def even_after_odd(head):
-#     return None
 
 
 def test_even_after_odd():
     print("--------------------------")
-    print("-------test_even_after_od-------")
+    print("-------test_even_after_odd-------")
     test_cases = [
         # degenerate cases first
-        # (None, []),
-        # ([], []),
+        (None, []),
+        ([], []),
         ([1], [1]),
-        ([1, 3], [1, 3]),           # only odds
+
+        ([1, 3], [1, 3]),             # only odds
+        ([3, 1], [3, 1]),             # only odds
+
         ([2, 4], [2, 4]),           # only even
+        ([4, 2], [4, 2]),           # only even
+
         ([1, 2], [1, 2]),           # even after odd
         ([2, 1], [1, 2]),           # even before odd
-        ([4, 1, 3], [1, 3, 4]),     # even at the beginning
-        ([4, 2, 3], [3, 4, 2]),     # odd at the end
-        ([4, 1, 2], [1, 4, 2]),     # odd between even
-        ([1, 2, 3], [1, 3, 2]),     # even between odd
-        ([1, 3, 5, 7], [1, 3, 5, 7]),
-        ([2, 4, 6, 8], [2, 4, 6, 8]),
-        ([1, 2, 3, 4, 5, 6], [1, 3, 5, 2, 4, 6]),
+
+        # ([4, 1, 3], [1, 3, 4]),     # even at the beginning
+        # ([4, 2, 3], [3, 4, 2]),     # odd at the end
+        # ([4, 1, 2], [1, 4, 2]),     # odd between even
+        # ([1, 2, 3], [1, 3, 2]),     # even between odd
+        # ([1, 3, 5, 7], [1, 3, 5, 7]),
+        # ([2, 4, 6, 8], [2, 4, 6, 8]),
+        # ([1, 2, 3, 4, 5, 6], [1, 3, 5, 2, 4, 6]),
 
         # specific cases
 
         # integration test
+
     ]
 
     for (args, answer) in test_cases:
@@ -120,3 +141,38 @@ def test_even_after_odd():
 
 
 test_even_after_odd()
+
+# Solution
+# def even_after_odd(current_node):
+#     if current_node is None:
+#         return create_linked_list([])
+#
+#     even = None
+#     odd = None
+#     even_tail = None
+#     head_tail = None
+#
+#     while current_node:
+#         next_node = current_node.next
+#
+#         if current_node.data % 2 == 0:
+#             if even is None:
+#                 even = current_node
+#                 even_tail = even
+#             else:
+#                 even_tail.next = current_node
+#                 even_tail = even_tail.next
+#         else:
+#             if odd is None:
+#                 odd = current_node
+#                 odd_tail = odd
+#             else:
+#                 odd_tail.next = current_node
+#                 odd_tail = odd_tail.next
+#         current_node.next = None
+#         current_node = next_node
+#
+#     if odd is None:
+#         return even
+#     odd_tail.next = even
+#     return odd
